0be8e7bb5f0345378781d9c1b92f85752fa9253e
oliviatroitino <olivia.troitino@live.u-tad.com>
2026-05-25 11:42:49 +0200

    feat(server): load and log tecnicas on startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fieldnotes/internal/content"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Cargar técnicas al arrancar
+	tecnicas, err := content.LoadTecnicas("content/tecnicas")
+	if err != nil {
+		log.Fatalf("Error cargando técnicas: %v", err)
+	}
+	log.Printf("Técnicas cargadas (%d):", len(tecnicas))
+	for _, t := range tecnicas {
+		log.Printf("  - %s [%s]", t.Nombre, t.Clasificacion)
+	}
+
 	mux := http.NewServeMux()
 
 	// Archivos estáticos
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	// Rutas (handlers a implementar en pasos siguientes)
+	// Rutas (handlers a completar en pasos siguientes)
 	mux.HandleFunc("/", homeHandler)
 
 	log.Println("Servidor arrancado en http://localhost:8080")
